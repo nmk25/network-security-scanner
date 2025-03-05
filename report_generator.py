d4631f02196331a702897e687700eb63db0c28d8
Nate Kadlec <46793440+nmk25@users.noreply.github.com>
2025-03-04 20:16:51 -0600

    Adds report_generator logic

--- report_generator.py
+++ report_generator.py
@@ -1,1 +1,19 @@
 # Generates scan reports
+
+import datetime
+
+def generate_report(scan_results, filename = "scan_report.txt"):
+    with open(filename, "w") as f:
+        f.write(f"Network Scan Report\n")
+        f.write(f"Generated on: {datetime.now()}\n\n")
+
+        for host, ports in scan_results.items():
+            f.write(f"Host: {host}\n")
+
+            for port, state in ports.items():
+                f.write(f"Port: {port}\tState: {state}\n\n")
+            
+            f.write("\n")
+    
+    print(f"Report saved to {filename}")
+    
